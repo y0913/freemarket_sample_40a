552dd38e638604aa33b2515d64b79b24572160c8
abeshi1019 <i>
2018-12-15 20:55:33 +0900

    create
    購入確認ページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   resources 'items'
   get 'mypage/card' => 'mypages#card'
   resources :mypages, path: 'mypage'
+
+  # これ必要な数とアクションでルーティングしないとrake routesがキモい
+  # resources :transactions, path: 'transaction/buy/:item_id'
+
+  resources :transactions, path: 'transaction/buy'
 end
