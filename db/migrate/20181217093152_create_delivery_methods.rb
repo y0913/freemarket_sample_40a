92046e295bbca6f9e6d3bb69e8491a8d549bc427
井筒 将之 <chopin0145122@yahoo.co.jp>
2018-12-17 19:51:44 +0900

    Revert "create_model"
    This reverts commit 289019f5bb32f49ecb0368aed71765b40bc16359.

--- db/migrate/20181217093152_create_delivery_methods.rb
+++
@@ -1,8 +0,0 @@
-class CreateDeliveryMethods < ActiveRecord::Migration[5.1]
-  def change
-    create_table :delivery_methods do |t|
-      t.string     :method, null: false
-      t.timestamps
-    end
-  end
-end
