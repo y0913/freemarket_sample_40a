3f2445c2248536deea297fe20a9658ef2786d607
井筒 将之 <chopin0145122@yahoo.co.jp>
2018-12-19 23:17:37 +0900

    bought_action
    Boughtアクションを定義

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,4 +2,8 @@ class TransactionsController < ApplicationController
   def index
     render :index, layout: "sub-layout"
   end
+
+  def bought
+    render :bought, layout: "sub-layout"
+  end
 end
