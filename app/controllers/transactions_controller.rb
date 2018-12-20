b09361a23366186fc2bd1fac53449e0ca51e2b6b
井筒 将之 <chopin0145122@yahoo.co.jp>
2018-12-20 15:13:52 +0900

    pay_action
    transactionコントローラーにpayアクションを追記

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,4 +2,14 @@ class TransactionsController < ApplicationController
   def index
     render :index, layout: "sub-layout"
   end
+
+  def pay
+    Payjp.api_key = 'sk_test_274b737e3ca8b2ce2925f882'
+    charge = Payjp::Charge.create(
+      :amount =>  1000,
+      :card => params['payjp-token'],
+      :currency => 'jpy',
+    )
+    redirect_to root_path, notice: 'ありがとうございました。'
+  end
 end
