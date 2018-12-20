b6a188615d6ecf93e719e3d6eedcb9ea3c6e5d0d
akira-yoshimura <a__kun@ezweb.ne.jp>
2018-12-20 15:04:50 +0900

    Edit items_controller_spec.rb
    #indexに必要ない為テスト削除しました

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,8 @@
 class ItemsController < ApplicationController
   def index
-  	@item = Item.all
+    @item = Item.all
     @items = Item.new
-
   end
-
   def new
     render :new, layout: "sub-layout"
   end
