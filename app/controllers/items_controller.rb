07502e154c911d38afb4aea6ddad842ccfda1719
y0913 <yohei19910825@gmail.com>
2018-12-21 14:23:48 +0900

    Made item-detail page
    商品詳細ページを作成いたしました。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,7 +6,17 @@ class ItemsController < ApplicationController
   def new
     render :new, layout: "sub-layout"
   end
+  def create
+  	Item.create(image: items_params[:image], text: items_params[:text], user_id: current_user.id)
+  end
   def show
   	@items = Item.new
+  	@item = Item.find(params[:id]) #指定したデータ取得できているかテストする
+    @user = User.find(params[:id])
+  end
+
+  private
+  def items_params
+  	params.require(:items).permit(:user_id, :text, :image, :id)
   end
 end
