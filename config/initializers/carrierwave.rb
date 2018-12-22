70e519c6d77d40640cacf9e25a873457ff8261c5
y0913 <yohei19910825@gmail.com>
2018-12-22 17:31:55 +0900

    Edit S3
    本番環境ではS3に、ローカル環境では/publicに保存されるように条件分岐しました。

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,9 +3,12 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
+  if Rails.env.development? || Rails.env.test?
+    config.storage = :file
+  elsif Rails.env.production?
+   config.storage = :fog
+   config.fog_provider = 'fog/aws'
+   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: Rails.application.secrets.aws_access_key_id,
     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
@@ -14,4 +17,5 @@ CarrierWave.configure do |config|
 
   config.fog_directory  = 'asia-osaka'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/asia-osaka'
+  end
 end
