b7ede176e340086ee4a733e68b05654ce1a81b04
abeshi1019 <i>
2018-12-24 17:57:59 +0900

    gem_minimagick

--- app/uploaders/images_uploader.rb
+++ app/uploaders/images_uploader.rb
@@ -2,7 +2,8 @@ class ImagesUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
-
+include CarrierWave::MiniMagick
+  process resize_to_fit: [300, 300]
   # Choose what kind of storage to use for this uploader:
   if Rails.env.development?
     storage :file
@@ -18,6 +19,10 @@ class ImagesUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+
+include CarrierWave::MiniMagick
+  process resize_to_fit: [300, 300]
+
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
