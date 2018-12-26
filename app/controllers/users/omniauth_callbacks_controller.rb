993376f60dcbaa0b0e5f65652d6450a36f329865
ChiakiMori <s.zero.s2590@gmail.com>
2018-12-26 17:14:30 +0900

    ADD users/omniauth_callbacks_controller.rb

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,25 @@
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+
+  def facebook
+    callback_from :facebook
+  end
+
+  def google_oauth2
+    callback_from(:google)
+  end
+
+  private
+  def callback_from(provider)
+    provider = provider.to_s
+    omi = request.env['omniauth.auth']
+    @user = User.find_for_oauth(omi)
+    if @user.persisted?
+      flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
+      sign_in_and_redirect @user, event: :authentication
+    else
+      session["devise.#{provider}_data"] = request.env['omniauth.auth']
+      redirect_to new_user_registration_url
+    end
+  end
+
+end
