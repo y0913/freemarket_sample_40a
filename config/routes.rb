22f4e7b3106e47ebe25bc2788f75de7357a4b5cc
ChiakiMori <s.zero.s2590@gmail.com>
2018-12-26 16:43:42 +0900

    EDIT routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks"}
   root 'items#index'
   resources 'items'
   get 'mypage/card' => 'mypages#card'
