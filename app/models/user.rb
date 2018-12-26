209dfe5ac4ecb5ea2db516d0b405a52c92eb6df4
ChiakiMori <s.zero.s2590@gmail.com>
2018-12-26 17:30:20 +0900

    EDIT user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,6 @@ class User < ApplicationRecord
   has_one :address
   has_many :items, dependent: :destroy
   has_many :transaction_users, dependent: :destroy
-  # has_many :transactions, through: :transaction_users, dependent: :destroy
   has_many :transaction_comments, dependent: :destroy
   has_many :rates
   has_many :likes
