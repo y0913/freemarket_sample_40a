0e2ab0ce9146f8ad2e369dd7ff6bb511855c97e0
井筒 将之 <chopin0145122@yahoo.co.jp>
2018-12-28 00:36:13 +0900

    fix_data
    データ等編集

--- db/schema.rb
+++ db/schema.rb
@@ -64,7 +64,7 @@ ActiveRecord::Schema.define(version: 20181226120153) do
 
   create_table "images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "item_id", null: false
-    t.string "image", default: "", null: false
+    t.string "image", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
