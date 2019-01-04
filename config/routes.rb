b3c64287b1cf91beff6a75f4817ee4a94a564eb8
y0913 <yohei19910825@gmail.com>
2019-01-04 15:25:05 +0900

    Resolve conflicts
    コンフリクトの修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks"}
   root 'items#index'
 
-  resources 'items' do
-    collection do
-      get 'search' => 'items#search'
-    end
-  end
-
+  resources 'items'
   post 'rate/:id' => 'rates#create'
   get 'mypage/card' => 'mypages#card'
   get 'mypage/card/create' => 'mypages#buy'
