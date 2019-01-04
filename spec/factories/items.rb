6e50f420f34147c0a025d71b99abee48149a0a18
y0913 <yohei19910825@gmail.com>
2019-01-04 14:35:14 +0900

    Edit あいまい検索
    あいまい検索の機能を実装しました。

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,23 +1,16 @@
 FactoryBot.define do
   factory :item do
-
     name                {'商品名'}
     description         {'商品説明です'}
-    category_id         {1}
-    postage_id          {1}
-    delivery_method_id  {2}
-    prefecture_id       {3}
-    delivery_day_id     {2}
-    price               {2}
-    condition_id        {2}
-    user_id             {1}
+    postage
+    delivery_method
+    category
+    prefecture
+    delivery_day
+    price               {500}
+    condition
+    user
+    item_state
   end
 
-#  後で書きます
-
-  # factory :abeshi, class: User do
-  #   id                {1}
-  #   nickname          {'あべし'}
-  #   email             {'abeshi@gmail.com'}
-  # end
 end
