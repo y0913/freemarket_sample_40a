ffd70c12380840e65382e127ed94f8e76efea76e
井筒 将之 <chopin0145122@yahoo.co.jp>
2019-01-04 11:29:56 +0900

    edit_routing
    ルーティングを編集

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks"}
   root 'items#index'
   resources 'items'
+  post 'rate/:id' => 'rates#create'
   get 'mypage/card' => 'mypages#card'
   get 'mypage/card/create' => 'mypages#buy'
   get 'mypage/logout' => 'mypages#logout'
