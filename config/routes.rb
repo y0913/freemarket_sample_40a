c129cca643e9b68b8d74fb460e257f37f5aa14fc
y0913 <yohei19910825@gmail.com>
2019-01-04 15:53:39 +0900

    Edit Routing
    ルーティングを修正しました。

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks"}
   root 'items#index'
 
-  resources 'items'
+  resources 'items' do
+    collection do
+      get 'search' => 'items#search'
+    end
+  end
+
+
   post 'rate/:id' => 'rates#create'
   get 'mypage/card' => 'mypages#card'
   get 'mypage/card/create' => 'mypages#buy'
