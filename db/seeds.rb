af03b9e4377c1a591be19f52e6fa2ec086b9b3d9
井筒 将之 <chopin0145122@yahoo.co.jp>
2019-01-07 17:56:35 +0900

    edit
    シードファイルの編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,7 @@ CSV.foreach('db/brand.csv') do |row|
 end
 
 CSV.foreach('db/category.csv') do |row|
-  Category.find_or_create_by(name: row[0], parent_id: row[1])
+  Category.find_or_create_by(name: row[0], parent_id: row[1], how_size: row[2])
 end
 
 # CSV.foreach('db/category_item.csv') do |row|
@@ -51,7 +51,7 @@ CSV.foreach('db/prefecture.csv') do |row|
 end
 
 CSV.foreach('db/size.csv') do |row|
-  Size.find_or_create_by(size: row[0])
+  Size.find_or_create_by(size: row[0], sizing: row[1])
 end
 
 CSV.foreach('db/transaction_state.csv') do |row|
