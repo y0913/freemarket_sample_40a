d63e7f8a9c956e1b7842caa0857681c588a2b04f
井筒 将之 <chopin0145122@yahoo.co.jp>
2019-01-10 01:23:56 +0900

    refactoring
    モデルからメソッドを呼び出す記述に編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -40,25 +40,7 @@ class ItemsController < ApplicationController
 
   def category
     @category = Category.find(params[:id])
-    @items = []
-    @categories = []
-    @categories << @category
-    if @category.children.present?
-      @category.children.each do |second_category|
-        @categories << second_category
-        case second_category.children.present?
-          when second_category.children.each do |third_category|
-            @categories << third_category
-          end
-        end
-      end
-    end
-    @categories.each do |category|
-      if category.items.present?
-        @items << category.items
-      end
-    end
-    @items = @items.flatten.sort_by{|i|i.created_at}.reverse
+    @items = Item.new.get_items_for(params[:id])
   end
 
   def destroy
