77241281c77661cb4054a2901a6b8ee36741f606
井筒 将之 <chopin0145122@yahoo.co.jp>
2019-01-10 01:23:31 +0900

    refactoring
    記述を変更したことによりコード量削減

--- app/models/item.rb
+++ app/models/item.rb
@@ -29,24 +29,11 @@ class Item < ApplicationRecord
 
   def get_items_for(id)
     category = Category.find(id)
-    items = []
-    categories = []
-    categories << category
-    if category.children.present?
-      category.children.each do |second_category|
-        categories << second_category
-        case second_category.children.present?
-          when second_category.children.each do |third_category|
-            categories << third_category
-          end
-        end
-      end
-    end
-    categories.each do |category|
-      if category.items.present?
-        items << category.items
-      end
-    end
-    items = items.flatten.sort_by{|i|i.created_at}.reverse
+    categories = [
+      category,
+      category.children,
+      category.children.map { |category| category.children }
+    ].flatten.compact
+    categories.map { |category| category.items }.flatten.sort_by{|i|i.created_at}.reverse
   end
 end
