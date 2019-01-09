befdaf3d057c42ea38b9734990cc5f2bd4d06405
井筒 将之 <chopin0145122@yahoo.co.jp>
2019-01-10 01:02:59 +0900

    edit_model
    モデルに親カテゴリーに紐づいたアイテムを取得するメソッドを定義

--- app/models/item.rb
+++ app/models/item.rb
@@ -26,4 +26,27 @@ class Item < ApplicationRecord
   validates :prefecture_id, presence: true
   validates :delivery_day_id, presence: true
   validates :price, presence: true,              numericality:{only_integer:true,greater_than_or_equal_to:300,less_than_or_equal_to:9999999}
+
+  def get_items_for(id)
+    category = Category.find(id)
+    items = []
+    categories = []
+    categories << category
+    if category.children.present?
+      category.children.each do |second_category|
+        categories << second_category
+        case second_category.children.present?
+          when second_category.children.each do |third_category|
+            categories << third_category
+          end
+        end
+      end
+    end
+    categories.each do |category|
+      if category.items.present?
+        items << category.items
+      end
+    end
+    items = items.flatten.sort_by{|i|i.created_at}.reverse
+  end
 end
