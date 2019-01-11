0dbfc626e44d5f07a9484605ad61b30f0c5f7408
井筒 将之 <chopin0145122@yahoo.co.jp>
2019-01-11 12:17:40 +0900

    edit
    スライダーがたまに表示されないエラーを修正

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,7 +1,7 @@
 $(document).on('turbolinks:load', function() {
   $(function(){
     if ($('.slider').length > 0) {
-      $(".slider").slick({
+      $(".slider").not('.slick-initialized').slick({
         autoplay: true,
         prevArrow: '<img src="https://www.mercari.com/jp/assets/img/common/jp/top/carousel-prev.png" class="slide-arrow prev-arrow">',
         nextArrow: '<img src="https://www.mercari.com/jp/assets/img/common/jp/top/carousel-next.png" class="slide-arrow next-arrow">'
