3a7b14fa6dff19cc1796a3cbecbe7aba098c6f56
井筒 将之 <chopin0145122@yahoo.co.jp>
2019-01-13 12:13:16 +0900

    edit
    ログインしていない状態で詳細ページに飛ぶと、ログイン画面に遷移するよう修正。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :set_item,only:[:edit,:update,:show,:destroy, :stop]
   before_action :user_collation,only:[:edit,:update,:destroy]
-  before_action :user_login,only:[:new]
+  before_action :user_login,only:[:new, :show]
 
   WOMAN = 1
   MAN = 2
